1d519a01b87f69f94c0c97350cdfaa8abbb16ed6
odinjaign <jaign@outlook.com>
2023-04-12 13:47:58 +0800

    增加update命令

--- src/default.nix
+++ src/default.nix
@@ -1,57 +1,37 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "root";
   home.homeDirectory = "/root";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "22.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
     pkgs.helix 
     pkgs.neofetch
+    
+    (pkgs.writeShellScriptBin "update" ''
+      [ -z $1 ] && {
+        echo "Please Give Flake directory"
+        exit 1
+      }
+      cd $1 && git add .
+      home-manager switch --flake $1
+    '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     ".bashrc".source = ./dotfiles/.bashrc;
     ".config/nix/nix.conf".text = ''
       substituters = https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store https://cache.nixos.org/
       experimental-features = nix-command flakes
     '';
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/root/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "hx";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   programs.git = {
