34d84098b2814ca3cbac7b4b1bac949149d51bf5
jaign <jaign@outlook.com>
2024-01-17 15:26:34 +0000

    add wsl flakes

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "jaign's flakes";
+
+  inputs = {
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+    
+  };
+  
+  outputs = { self
+    , nixpkgs
+    , home-manager
+    , nixos-wsl
+    ,... } @inputs : {
+    nixosConfigurations = {
+      "nixos-wsl" = let
+	      username = "jaign"; 
+        hostname = "nixos-wsl";
+      in nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs username hostname; };
+        modules = [
+          # nixos-wsl.nixosModules.wsl
+          # home-manager.nixosModules.home-manager
+          ./nixos-wsl.nix          
+        ];
+      };
+    };
+
+    homeConfigurations = {
+      "jaign" = let 
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+      in home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home-manager ];
+      };
+    };
+  };
+}
---
+++ home-manager/cli.nix
@@ -0,0 +1,11 @@
+{pkgs, ...}: {
+  
+  programs = {
+    git = {
+      enable = true;
+      userName = "jaign";
+      userEmail = "jaign@outlook.com";
+    };
+  };
+
+}
